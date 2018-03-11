d17389fce4742909a41856c1f44a225581dc8e3e
sos171807fjpz <franciscojesusperez@hotmail.com>
2018-03-11 17:47:33 +0000

    Modificado fichero index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express");
 
 var app = express();
 
+app.use("/",express.static("/home/ubuntu/workspace/SOS1718-07/public"));
+
 app.get("/hello",(req,res) =>{
     res.send("hello World!");
 });
