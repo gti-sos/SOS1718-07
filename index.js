aba9436a6787e5a3e13622c3ddceba3f4667d154
Miguel Angel Gomariz Cantador <miguelgia@hotmail.com>
2018-03-17 18:13:04 +0000

    cambios

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var app = express();
 
 app.use("/",express.static( path.join(__dirname,"public")));
 
-app.use("/api/v1",express.static(path.join(__dirname,"global-terrorism-data")))
+app.use("/api/v1/global-terrorism-data",express.static(path.join(__dirname,"global-terrorism-data")));
+app.use("/api/v1/attacks-data",express.static(path.join(__dirname,"attacks-data")));
+app.use("/api/v1/homicide-reports-data",express.static(path.join(__dirname,"homicide-reports-data")));
 
 app.get("/hello",(req,res) =>{
     res.send("hello World!");
