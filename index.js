5f67583b5c4e73106b2f71b6fc20b2145fe570fc
Miguel Angel Gomariz Cantador <miguelgia@hotmail.com>
2018-03-17 18:07:19 +0000

    Estructura F04

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var app = express();
 
 app.use("/",express.static( path.join(__dirname,"public")));
 
+app.use("/api/v1",express.static(path.join(__dirname,"global-terrorism-data")))
+
 app.get("/hello",(req,res) =>{
     res.send("hello World!");
 });
