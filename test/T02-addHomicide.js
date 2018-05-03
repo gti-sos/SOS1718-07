3152666abe0d6028c0a01fcfe6e2b93f602a9455
sos171807fjpz <franciscojesusperez@hotmail.com>
2018-05-03 11:37:34 +0000

    cambios test

--- test/T02-addHomicide.js
+++ test/T02-addHomicide.js
@@ -5,37 +5,30 @@
 
 
 
-describe('Add homicide', function(){  //compruebo que los datos se cargan
-    it('should show same homicides', function(){ //para cada una de las comprobaciones meto la palabra it
+describe('Add homicide', function(){  //compruebo que se añade un homicidio
+    it('should add a new homicide', function(){ //para cada una de las comprobaciones meto la palabra it
         browser
             .get('https://sos171807fjpz-sandbox2-sos171807fjpz.c9users.io/terrorismApp.html#!/homicide-reports-data')  //accede a la direccion
-            .then(function(){
+          //  .then(function(){
                 element
                     .all(by.repeater('homicide in homicides'))  //te devuelve todos los elementos de la pagina que ha cargado que cumplan una condicion, que contengan el ng-repeat
-                    .then(function(homicides){ //carga todos los homicidios
-                    
+                    .then(function(initialHomicides){ //carga todos los homicidios
                         browser.driver.sleep(2000);
                         
-                        element(by.model('newHomicide.year')).sendKeys(2342);
+                        element(by.model('newHomicide.year')).sendKeys(2345);
                         element(by.model('newHomicide.month')).sendKeys('July');
                         element(by.model('newHomicide.state')).sendKeys('Alabama');
                         element(by.model('newHomicide.city')).sendKeys('Anchorage');
                         element(by.model('newHomicide.crime_type')).sendKeys('Murder');
                         element(by.model('newHomicide.weapon')).sendKeys('Rifle');
-                        element(by.model('newHomicide.victim_count')).sendKeys('100');
+                        element(by.model('newHomicide.victim_count')).sendKeys(100);
                         
                         element(by.buttonText('Add')).click().then(function(){
-                            
-                            element.all(by.repeater('homicide in homicides').then(function(homicides){ 
-                                expect(homicides.length).toEqual(homicides.length+1);
-                            
+                            element.all(by.repeater('homicide in homicides')).then(function (homicides){
+                                expect(homicides.length).toEqual(initialHomicides.length+1);
                             });
-                        
-                        
-                       
-                    });
-            });
-        
-       
+                        });
+                });            
+         //  });
     });
 });
