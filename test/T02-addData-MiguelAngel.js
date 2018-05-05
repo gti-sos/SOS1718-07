8e2c47f11d807e035e30ae119c09eadf4de50962
Miguel Angel Gomariz Cantador <miguelgia@hotmail.com>
2018-05-05 10:58:36 +0000

    Pruebas anyadidas

---
+++ test/T02-addData-MiguelAngel.js
@@ -0,0 +1,35 @@
+/*global expect*/
+/*global browser*/
+/*global element*/
+/*global by*/
+
+var fs = require("fs");
+var path = require("path");
+
+describe('Add new Data', function() {
+    it('should add a new data', function() {
+        browser
+            .get('https://personal-miggomcan-sos171807magc.c9users.io/terrorismApp.html#!/global-terrorism-data')
+            .then(function() {
+                element.all(by.repeater('data in globalTerrorismData'))
+                    .then(function(initialData) {
+
+                        element(by.model('newData.country_txt')).sendKeys('Spain');
+                        element(by.model('newData.iyear')).sendKeys(Math.random());
+                        element(by.model('newData.imonth')).sendKeys(11);
+                        element(by.model('newData.iday')).sendKeys(1);
+                        element(by.model('newData.city')).sendKeys('Sevilla');
+                        element(by.model('newData.attacktype_txt')).sendKeys("Bomb");
+                        element(by.model('newData.weaptype_txt')).sendKeys('Explosives');
+                        element(by.model('newData.nkill')).sendKeys(Math.random());
+
+                        element(by.buttonText('Add')).click().then(function() {
+                            element.all(by.repeater('data in globalTerrorismData')).then(function(globalTerrorismData) {
+                                expect(globalTerrorismData.length).toBeGreaterThanOrEqual(initialData.length);
+                            });
+
+                        });
+                    });
+            });
+    });
+});
