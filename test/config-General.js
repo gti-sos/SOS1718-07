c131fdbbbf6b7e50913ac1fe8486608b356e237b
Miguel Angel Gomariz Cantador <miguelgia@hotmail.com>
2018-05-24 13:43:00 +0000

    pruebas automaticas

---
+++ test/config-General.js
@@ -0,0 +1,11 @@
+exports.config = {   //exportar objeto de la configuracion del test
+
+    seleniumAddress : 'http://localhost:8910',   //url donde esta el navegador cargando
+    
+    specs: ['T01-loadData-Ismael.js','T02-addData-Ismael.js', "T01-loadData-MiguelAngel.js","T02-addData-MiguelAngel.js", "T01-loadData.js", "T02-addHomicide"],  //test que vamos a cargar
+    
+    capabilites : {   //tipo de navegador que voy a usar
+        'browserName' : 'phantomjs'
+        
+    }
+};
