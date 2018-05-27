2c786dc2ecbcba9b72def8ef25100d01cbac84d6
sos171807iag <ismaelwf25@gmail.com>
2018-05-27 13:01:02 +0000

    pruebas auto

--- test/config-General.js
+++ test/config-General.js
@@ -3,9 +3,9 @@ exports.config = {   //exportar objeto de la configuracion del test
     seleniumAddress : 'http://localhost:8910',   //url donde esta el navegador cargando
     
     specs: ['T00-Api-MiguelAngel.js','T00-API-FRAN.js', 'T00-API-Ismael.js',
-    'T01-loadData-Ismael.js','T02-addData-Ismael.js', 
     "T01-loadData-MiguelAngel.js","T02-addData-MiguelAngel.js", 
-    "T01-loadData.js", "T02-addHomicide"],  //test que vamos a cargar
+    "T01-loadData.js", "T02-addHomicide.js",
+    'T01-loadData-Ismael.js','T02-addData-Ismael.js'],  //test que vamos a cargar
     
     capabilites : {   //tipo de navegador que voy a usar
         'browserName' : 'phantomjs'
