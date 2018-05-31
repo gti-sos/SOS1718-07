dc2ba26ecf77651276cc1c4ad2647f3a0a976a60
Miguel Angel Gomariz Cantador <miguelgia@hotmail.com>
2018-05-31 19:42:17 +0000

    ultima integracion

--- public/vistaIntegracionMiguelAngelCtrl.js
+++ public/vistaIntegracionMiguelAngelCtrl.js
@@ -527,59 +527,39 @@ angular.module("TerrorismManagerApp").controller("vistaIntegracionMiguelAngelCtr
     $http.get("api/v1/global-Terrorism-Data").then(function doneFilter(responseMia) {
         $http.get("https://mcapi.ca/blockedservers").then(function doneFilter(responseExterna) {
             var misDatos = [];
-            var datosExternos = [];
 
             for (var i = 0; i < responseMia.data.length; i++) {
                 var object = {};
-                object["title"] = datosExternos.push(responseExterna.data.blocked[i]);
+                object["title"] = responseExterna.data["blocked"][i];
                 object["value"] = responseMia.data[i].nkill;
                 misDatos.push(object);
             }
+            var chart = AmCharts.makeChart("quintaIntegracionExterna", {
+                "type": "funnel",
+                "theme": "light",
+                "dataProvider": misDatos,
+                "titleField": "title",
+                "marginRight": 160,
+                "marginLeft": 15,
+                "labelPosition": "right",
+                "funnelAlpha": 0.9,
+                "valueField": "value",
+                "startX": 0,
+                "neckWidth": "40%",
+                "startAlpha": 0,
+                "outlineThickness": 1,
+                "neckHeight": "30%",
+                "balloonText": "[[title]]:<b>[[value]]</b>",
+                "export": {
+                    "enabled": true
+                }
+            });
 
 
         });
     });
 
-    var chart = AmCharts.makeChart("chartdiv", {
-        "type": "funnel",
-        "theme": "light",
-        "dataProvider": [{
-            "title": "Website visits",
-            "value": 300
-        }, {
-            "title": "Downloads",
-            "value": 123
-        }, {
-            "title": "Requested price list",
-            "value": 98
-        }, {
-            "title": "Contaced for more info",
-            "value": 72
-        }, {
-            "title": "Purchased",
-            "value": 35
-        }, {
-            "title": "Contacted for support",
-            "value": 15
-        }, {
-            "title": "Purchased additional products",
-            "value": 8
-        }],
-        "balloon": {
-            "fixedPosition": true
-        },
-        "valueField": "value",
-        "titleField": "title",
-        "marginRight": 240,
-        "marginLeft": 50,
-        "startX": -500,
-        "rotate": true,
-        "labelPosition": "right",
-        "balloonText": "[[title]]: [[value]]n[[description]]",
-        "export": {
-            "enabled": true
-        }
-    });
+
 
 
 
